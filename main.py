877414ce71cd117369efec3a3885c14ec890ea5e
Eduardo Henrique <duksduks@gmail.com>
2018-09-11 16:25:34 -0300

    main just start the app

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import window
+import photopobre
 
-im = window.Image.open("Space_187k.jpg")#.save("filhocopy.jpg")
-
-ima = window.Gtk.Image.new_from_pixbuf(window.image2pixbuf(im))
-win = window.OriginalWindow()
-windows = list()
-images = list()
-images.append(im)
-windows.append(win)
-win.connect("destroy", window.Gtk.main_quit)
-win.add(ima)
-win.show_all()
-
-win = window.ButtonWindow()
-windows.append(win)
-win.connect("destroy", window.Gtk.main_quit)
-#win.add(ima)
-win.show_all()
-
-win = window.CopyWindow()
-windows.append(win)
-win.connect("destroy", window.Gtk.main_quit)
-win.show_all()
-
-window.Gtk.main()
+app = photopobre.PhotoPobre()
+app.run()
